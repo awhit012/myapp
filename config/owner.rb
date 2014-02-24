dce13012d2a27ab2770ffa2f8224ec06b7544e6e
Alex White <awhit012@gmail.com>
2014-02-23 20:54:32 -0800

    add pages Services and Portfolio

--- config/owner.rb
+++ config/owner.rb
@@ -1,11 +1,11 @@
 class Owner
 
 	def name
-	   	name = 'Foobar Kadigan'
+	   	name = 'Alex White'
    	end
 
    	def birthdate
-   		birthdate = Date.new(1990, 12, 22)
+   		birthdate = Date.new(1984, 05, 30)
 	end
 
 	def countdown
